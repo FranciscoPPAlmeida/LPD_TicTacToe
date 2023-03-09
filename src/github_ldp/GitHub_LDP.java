7b5e9f496fd583f5ae8fee9ec16c6b1f86a38363
Rui-Puga <72913722+Rui-Puga@users.noreply.github.com>
2023-03-09 12:34:14 +0000

    added a comment

--- src/github_ldp/GitHub_LDP.java
+++ src/github_ldp/GitHub_LDP.java
@@ -16,7 +16,7 @@ public class GitHub_LDP {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //chamussas SA
+        //chamussas SAl
     }
     
 }
